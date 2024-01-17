31c499dd44add1837d9cc65e16c1d20e7ce292d8
0leksandrr <tsybulskyy.sasha@gmail.com>
2024-01-17 20:59:46 +0200

    downloading files

--- resource/urls.py
+++ resource/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import resource  
+from .views import resource, download_pdf
 
 urlpatterns = [
-    path('', resource, name='resource'),  
+    path('', resource, name='resource'),
+    path('download/<int:pk>/', download_pdf, name='download_pdf'),
 ]
--- resource/views.py
+++ resource/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import FileResponse
+from .forms import PDFFileForm
+from .models import PDFFile
 
 def resource(request):
-    return render(request, 'main/resources.html')
+    pdf_files = PDFFile.objects.all()  # Отримуємо всі PDF-файли з бази даних
+    if request.method == 'POST':
+        form = PDFFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home') 
+    else:
+        form = PDFFileForm()
+
+    return render(request, 'main/resources.html', {'form': form, 'pdf_files': pdf_files})
+
+
+def download_pdf(request, pk):
+    pdf_file = get_object_or_404(PDFFile, pk=pk)
+    response = FileResponse(pdf_file.file, as_attachment=True)
+    return response
+
